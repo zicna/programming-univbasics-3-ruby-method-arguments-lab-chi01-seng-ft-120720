972d57f637b31a8c39eef6ab627e14508d014c62
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-15 19:03:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-
+puts "Hello"
 
 
 
